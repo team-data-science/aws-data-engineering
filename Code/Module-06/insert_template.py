6fa923c38c82871124d5f904e3cacabb43921042
Team Data Science Andreas Kretz <66935316+team-data-science@users.noreply.github.com>
2022-02-04 07:35:38 +0100

    Added testfile

--- Code/Module-06/insert_template.py
+++ Code/Module-06/insert_template.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import requests
 
+'''
+Update 2021-02-04: Created the TestSample.csv file in the new data folder and added the path to it here . This way you don't have to create it yourself
+'''
+
 # URL of your endpoint
 URL = "https://xxxxxx.execute-api.us-east-1.amazonaws.com/prod/hello"
 
+
 #read the testfile
-data = pd.read_csv('TestSample.csv', sep = ',')
+data = pd.read_csv('data/TestSample.csv', sep = ',')
 
 # write a single row from the testfile into the api
 #export = data.loc[2].to_json()
